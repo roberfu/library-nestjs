6e882e2a459006c35a61a998a986cc3846fed9c7
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2025-05-14 11:45:34 -0400

    feat: Add fields to author entity

--- src/authors/entities/author.entity.ts
+++ src/authors/entities/author.entity.ts
@@ -1,1 +1,17 @@
-export class Author {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Author {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    yearOfBirth: number;
+
+    @Column()
+    nationality: string;
+}
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class Book {
 
-    @PrimaryGeneratedColumn('identity')
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Column('text', { unique: true })
